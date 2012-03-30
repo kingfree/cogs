53b07225bf4b10e97e863012c841c0a968ddce82
Kingfree <anytjf@gmail.com>
2012-03-30 09:21:27 +0800

    修改题目界面

--- include/env.inc.php
+++
@@ -1,8 +0,0 @@
-<?php $cfg=array (
-  'data_server' => 'localhost',
-  'data_database' => 'cojs',
-  'data_uid' => 'cogs',
-  'data_pwd' => 'NDab3cZqFAJMpMe8',
-  'dir_root' => 'cogs/',
-  'safety_sn' => 'CmYkRgB123321BgRkYmC',
-) ?>
