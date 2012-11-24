34a3f7d6a67c93de911975c1e53d983b2b12491b
KingFree <anytjf@gmail.com>
2012-11-24 13:46:21 +0800

    ＯＩ不再

--- include/footer.php
+++ include/footer.php
@@ -9,11 +9,11 @@
 <a class='btn btn-small' onclick="document.getElementById('trad').innerHTML='中文繁體';document.getElementById('alltext').innerHTML = TradSimp.getSimp(document.getElementById('alltext').innerHTML);" id="simp">中文简体</a>
 </div>
 <p>
-应用 <a href="http://twitter.github.com/bootstrap/" target="_blank">Twitter Bootstrap</a> 作为界面框架，应用了 <a href="/Bootstrap/css/<?=$_SESSION['user_style']?>.min.css" target="_blank"><?=$_SESSION['user_style']?>.min.css</a> 主题。
+以 <a href="http://twitter.github.com/bootstrap/" target="_blank">Twitter Bootstrap</a> 作为界面框架，应用 <a href="/Bootstrap/css/<?=$_SESSION['user_style']?>.min.css" target="_blank"><?=$_SESSION['user_style']?></a> 主题。
 <?php echo 输出文本("进程运行 %processtime% s ，处理完成数据库 %querytimes% 次。") ?>
 </p>
 <?php echo 输出文本($SET['global_tail']) ?>
-由 <a href="https://github.com/KingFree/COGS-by-Kingfree" target="_blank">CmYkRgB123 在线评测系统</a> 强力驱动，版本 <?php echo $cfg['Version']; ?> ，目前由 <a href="http://anytjf.diandian.com" target="_blank">王者自由</a> 维护。版权所有 &copy; <a href="http://www.byvoid.com" target="_blank">BYVoid</a>，保留部分权利。
+由 <a href="https://github.com/KingFree/COGS-by-Kingfree" target="_blank">CmYkRgB123 在线评测系统</a> 强力驱动，版本 <?php echo $cfg['Version']; ?> ，由 <a href="http://anytjf.diandian.com" target="_blank">王者自由</a> 二次开发。原作者 &copy; <a href="http://www.byvoid.com" target="_blank">BYVoid</a>，保留部分权利。
 </p>
 </div>
 </div>
